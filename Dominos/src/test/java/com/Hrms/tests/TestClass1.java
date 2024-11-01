6f13cd1aeb2a0d190d648975d21137b07019d4e4
mahendrababukaranam <mahindra.karanam@gmail.com>
2024-11-01 10:37:27 +0530

    Worked on review comments

--- Dominos/src/test/java/com/Hrms/tests/TestClass1.java
+++ Dominos/src/test/java/com/Hrms/tests/TestClass1.java
@@ -4,6 +4,8 @@ public class TestClass1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
+		System.out.println("Hello Mahendra");
+		System.out.println("Worked on Review Comments");
 	}
 
 }
